fca8a9ea14a5b227bd1c4d9bddea322dd90200f9
Mark Clu Heramis <chumheramis@gmail.com>
2022-10-21 04:35:16 +0800

    add error handling
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- script.js
+++ script.js
@@ -8,38 +8,49 @@ var result = [];
 if (require.main === module) {
     const [, , ...args] = process.argv;
     const file = args[0];
-    var data = fs.readFileSync(file);
-    // convert data to array buffer
-    var arrayBuffer = new Uint8Array(data).buffer;
-    // parse
-    var dicomParser = new dwv.dicom.DicomParser();
-    dicomParser.parse(arrayBuffer);
-
-    var tags = dicomParser.getDicomElements();
-    const excludedTag = [
-        "PixelData"
-    ]
-    const vrExcluded = [
-        "UV", "UN", "SQ"
-    ];
-    Object.entries(dicomDataDictionary.standardDataElements).forEach(tag => {
-        const [tagKey, tagValue] = tag;
-        var item = tags.getFromName(tagValue.name);
-        if (
-            !(item == null || item == undefined || item == "") &&
-            excludedTag.indexOf(tagValue.name) === -1 &&
-            vrExcluded.indexOf(tagValue.vr) === -1
-        ) {
-            result[tagKey] = {
-                vr: tagValue.vr,
-                tag: tagValue.name,
-                value: item
-            };
-
+    if (!fs.existsSync(file)) {
+        console.log({
+            error: true,
+            message: `File ${file} does not exists`
+        })
+    } else {
+        var data = fs.readFileSync(file);
+        // convert data to array buffer
+        var arrayBuffer = new Uint8Array(data).buffer;
+        // parse
+        var dicomParser = new dwv.dicom.DicomParser();
+        try {
+            dicomParser.parse(arrayBuffer);
+            var tags = dicomParser.getDicomElements();
+            const excludedTag = [
+                "PixelData"
+            ]
+            const vrExcluded = [
+                "UV", "UN", "SQ"
+            ];
+            Object.entries(dicomDataDictionary.standardDataElements).forEach(tag => {
+                const [tagKey, tagValue] = tag;
+                var item = tags.getFromName(tagValue.name);
+                if (
+                    !(item == null || item == undefined || item == "") &&
+                    excludedTag.indexOf(tagValue.name) === -1 &&
+                    vrExcluded.indexOf(tagValue.vr) === -1
+                ) {
+                    result[tagKey] = {
+                        vr: tagValue.vr,
+                        tag: tagValue.name,
+                        value: item
+                    };
+                }
+            });
+            console.log(result)
+        } catch (error) { 
+            console.error({
+                code: true,
+                message: error.message
+            })
         }
-    });
-    console.log(result)
-    
+    }
 } else {
     console.log('required as a module');
 }
